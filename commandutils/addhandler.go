14d06db1b4cebb125da2fa520971bd095fb20589
Patrice Vanpaemel <pvanpmails@gmail.com>
2025-02-18 21:26:24 +0100

    Changed add

--- commandutils/addhandler.go
+++ commandutils/addhandler.go
@@ -3,15 +3,20 @@ package commandutils
 import (
 	"encoding/csv"
 	"fmt"
+	"strconv"
 
 	"example.org/to-do-app/fileutils"
 )
 
 func AddTask(taskName string) {
 	file := fileutils.GetPage()
-
 	writer := csv.NewWriter(file)
-	newRow := []string{taskName}
+	reader := csv.NewReader(file)
+
+	lastTodo := readLastLine(reader)
+	id := getNextId(lastTodo)
+
+	newRow := []string{id, taskName, "Aktiv"}
 
 	err := writer.Write(newRow)
 
@@ -25,4 +30,36 @@ func AddTask(taskName string) {
 	if err := writer.Error(); err != nil {
 		fmt.Println("Fehler beim Flushen:", err)
 	}
+}
+
+func readLastLine(reader *csv.Reader) []string {
+	var lastTodo []string
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break // EOF oder Fehler
+		}
+		lastTodo = record
+	}
+
+	return lastTodo
+}
+
+func getNextId(lastTodo []string) string {
+	var id string
+
+	if len(lastTodo) == 0 {
+		id = "1"
+	} else {
+		tmpId, err := strconv.Atoi(lastTodo[0])
+
+		if err != nil {
+			fmt.Println("Fehler biem Konvertieren der ID:", err)
+		}
+
+		id = strconv.Itoa(tmpId + 1)
+	}
+
+	return id
 }
--- fileutils/filehandler.go
+++ fileutils/filehandler.go
@@ -3,14 +3,14 @@ package fileutils
 import "os"
 
 func GetPage() *os.File {
-	file, err := os.OpenFile("example.csv", os.O_APPEND, 0666)
+	file, err := os.OpenFile("todo.csv", os.O_APPEND, 0666)
 
 	return createFile(err, file)
 }
 
 func createFile(err error, file *os.File) *os.File {
 	if err != nil {
-		file, err := os.Create("example.csv")
+		file, err := os.Create("todo.csv")
 
 		if err != nil {
 			panic(err)
